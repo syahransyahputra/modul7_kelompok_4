1a7da0a11b56106694b4059536d57a028a22094d
rimuri03 <muhammadzulfadly@student.telkomuniversity.ac.id>
2023-04-04 17:11:36 +0700

    menambahkan class DataMahasiswa1302210046

---
+++ modul7_kelompok_4/modul7_kelompok_4/DataMahasiswa1302210046.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Text.Json;
+
+namespace modul7_kelompok_4
+{
+    public class DataMahasiswa1302210046
+    {
+	    public void ReadJSON()
+            {
+                String jsonString = File.ReadAllText("D:\\modul7_kelompok_4\\modul7_kelompok_4\\modul7_kelompok_4\\jurnal7_1_1302210046.json");
+
+                Mahasiswa mahasiswa = JsonSerializer.Deserialize<Mahasiswa>(jsonString);
+
+            Console.WriteLine("Nama: " + mahasiswa.firstName + " " + mahasiswa.lastName);
+            Console.WriteLine("Jenis Kelamin: " + mahasiswa.gender);
+            Console.WriteLine("Umur: " + mahasiswa.age);
+            Console.WriteLine("Alamat: " + mahasiswa.address.streetAddress +" " + mahasiswa.address.city + " " + mahasiswa.address.state);
+            Console.WriteLine("");
+            Console.WriteLine("MataKuliah Yang di ambil:");
+            for(int i = 0; i < mahasiswa.courses.Count; i++)
+            {
+                Console.WriteLine("Code: " + mahasiswa.courses[i].code + " MataKuliah: " + mahasiswa.courses[i].name);
+            }
+                
+        }
+ 
+
+        public class Mahasiswa
+        {
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public string gender { get; set; }
+            public int age { get; set; }
+            public Address address { get; set; }
+            public List<Courses> courses { get; set; }
+            public Mahasiswa() { }
+            public Mahasiswa(string firstName, string lastName, string gender, int age, Address address, List<Courses> courses)
+            {
+                this.firstName = firstName;
+                this.lastName = lastName;
+                this.gender = gender;
+                this.age = age;
+                this.address = address;
+                this.courses = courses;
+            }
+        }
+
+        public class Address
+        {
+            public string streetAddress { get; set; }
+            public string city { get; set; }
+            public string state { get; set; }
+            public Address() { }
+            public Address(string streetAddress, string city, string state)
+            {
+                this.streetAddress = streetAddress;
+                this.city = city;
+                this.state = state;
+            }
+        }
+
+        public class Courses
+        {
+            public string code { get; set;}
+            public string name { get; set;}
+        }
+    }
+}
--- modul7_kelompok_4/modul7_kelompok_4/Program.cs
+++ modul7_kelompok_4/modul7_kelompok_4/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+namespace modul7_kelompok_4
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            DataMahasiswa1302210046 dataMHS = new DataMahasiswa1302210046();
+            dataMHS.ReadJSON();
+        }
+    }
+}
